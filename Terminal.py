f5b47b734f01e3bfac24e98ab4402395d4d1782b
RiptideV <84823412+RiptideV@users.noreply.github.com>
2026-06-22 10:47:07 -0700

    Added new options and complete battle system.

--- Terminal.py
+++ Terminal.py
@@ -14,10 +14,15 @@ rules = False
 key = False
 fight = False
 standing = True
+buy = False
+speak = False
+boss = False
+
+
 
 HP = 100
 HPMAX = HP
-ATK = 3
+ATK = 1
 pot = 1
 elx = 1
 gold = 0
@@ -27,7 +32,7 @@ y = 0
 map = [["plains",  "plains",   "empty",   "plains",  "forest", "mountain",       "cave"],  # y = 0
        ["forest",  "forest",   "forest",   "forest",  "forest",    "hills",   "mountain"],  # y = 1
        ["forest",  "fields",   "bridge",   "plains",   "hills",   "forest",      "hills"],  # y = 2
-       ["plains",    "shop",     "town",    "major",  "plains",    "hills",   "mountain"],  # y = 3
+       ["plains",    "shop",     "town",    "mayor",  "plains",    "hills",   "mountain"],  # y = 3
        ["plains",  "fields",   "fields",   "plains",   "hills", "mountain",   "mountain"]]  # y = 4
 
 y_len = len(map)-1
@@ -75,27 +80,27 @@ mobs = {
     "Skeleton": {
         "hp": 20,
         "atk": 5,
-        "gold": 5
+        "g": 5
     },
     "Goblin": {
         "hp": 15,
         "atk": 3,
-        "gold": 3
+        "g": 3
     },
     "Zombie": {
         "hp": 30,
         "atk": 4,
-        "gold": 7
+        "g": 7
     },
     "Ghost": {
         "hp": 25,
         "atk": 6,
-        "gold": 10
+        "g": 10
     },
     "Mummy": {
         "hp": 35,
         "atk": 7,
-        "gold": 15
+        "g": 15
     }
 }
 
@@ -105,17 +110,32 @@ print(current_tile)
 name_of_tile = biome[current_tile]["t"]
 enemy_tile = biome[current_tile]["e"]
 
+dmgnum1 = 0
+dmgnum2 = 0
+dmgnum3 = 0
+dmgnum4 = 0
+dmgnum5 = 0
+dmgnum6 = 0
+dmgnum7 = 0
+dmgnum8 = 0
+dmgnum9 = 0
+dmgnum10 = 0
+
+
+
+
 weapon = {
-    "sword": {"damage": [1, 2, 3, 4, 5, 6, 7, 8, 9]},
-    "axe": {"damage": [2, 3, 4, 5, 6, 9, 8, 9, 10]},
+    "sword": {"damage": [dmgnum1 + 1, dmgnum2 + 3, dmgnum3 + 3, dmgnum4 + 4, dmgnum5 + 5, dmgnum6 + 6, dmgnum7 + 6, dmgnum8 + 6, dmgnum9 + 6], "delay": 0.1},
+    "axe": {"damage": [dmgnum1 + 1, dmgnum2 + 1, dmgnum3 + 1, dmgnum4 + 3, dmgnum5 + 6, dmgnum6 + 8, dmgnum7 + 8, dmgnum8 + 9, dmgnum9 + 10], "delay": 0.12},
+    "dagger": {"damage": [dmgnum1 + 1, dmgnum2 + 1, dmgnum3 + 2, dmgnum4 + 2, dmgnum5 + 12], "delay": 0.05}
 }
 
-damage_list = weapon["axe"]["damage"]   # change to "sword" or "axe"
+damage_list = weapon["dagger"]["damage"]   # change to "sword" or "axe"
+weapon_delay = weapon["dagger"]["delay"]   # change to "sword" or "axe"
 dmgindex = 0
 
 move_delay = 0.12
 last_move = 0
-
 
 
 
@@ -148,6 +168,15 @@ def save():
         f.write(item + "\n")
     f.close()
 
+def heal(amount):
+    global HP
+    if HP + amount > HPMAX:
+        HP += amount
+    else:
+        HP = HPMAX
+    print(name + " healed " + str(amount) + " HP.")
+
+
 def build_screen(index):
     damage_strings = [str(x) for x in damage_list]
     dmgvalues = "Damage: " + " ".join(damage_strings)
@@ -166,6 +195,7 @@ def build_screen(index):
 def draw_screen(index):
     clear()
     dmgvalues, arrow_line = build_screen(index)
+    print("Press ENTER to attack.")
     print(dmgvalues)
     print(arrow_line)
 
@@ -178,7 +208,7 @@ def battle():
     hp = mobs[enemy]["hp"]
     hpmax = hp
     atk = mobs[enemy]["atk"]
-    gold = mobs[enemy]["gold"]
+    g = mobs[enemy]["g"]
 
     while fight:
         clear()
@@ -203,6 +233,7 @@ def battle():
         if choice == "1":
             draw_screen(dmgindex)
             rolling = True
+            move_delay = weapon_delay
             while rolling:
                 now = time.time()
 
@@ -210,8 +241,15 @@ def battle():
                     char = msvcrt.getch()
                     if char == b'\r':
                         clear()
-                        print("Damage:", damage_list[dmgindex])
+                        print(name + " dealt " + str(damage_list[dmgindex]) + " + " + str(ATK) + " damage to " + enemy)
                         rolling = False
+                        time.sleep(1)
+                        hp -= damage_list[dmgindex] + ATK
+                        if hp > 0:
+                            HP -= atk
+                            print(enemy + " dealt " + str(atk) + " damage to " + name)
+                            input("> ")
+                        
 
                 if now - last_move >= move_delay:
                     dmgindex = (dmgindex + 1) % len(damage_list)
@@ -219,6 +257,53 @@ def battle():
                     last_move = now
 
                 time.sleep(0.01)
+        elif choice == "2":
+                if pot > 0:
+                    pot -= 1
+                    heal(30)
+                    hp -= atk
+                    print(enemy + " dealt " + str(atk) + " damage to " + name)
+                else:
+                    print("You have no potions left.")
+                input("> ")
+                    
+        elif choice == "3":
+                if elx > 0:
+                    elx -= 1
+                    heal(50)
+                    hp -= atk
+                    print(enemy + " dealt " + str(atk) + " damage to " + name)
+                else:
+                    print("You have no elixirs left.")
+                input("> ")
+
+        if HP <= 0:
+            clear()
+            draw()
+            print(enemy + " defeated " + name + ".")
+            draw()
+            fight = False
+            play = False
+            print("GAME OVER")
+            quit()
+        if hp <= 0:
+            print(name + " defeated the " + enemy + "!")
+            draw()
+            fight = False
+            gold += g
+            print("You found " + str(gold) + " gold on the " + enemy + ".")
+            if random.randint(0, 100) <= 10:  # 20% chance of potion drop
+                pot += 1
+                print("The " + enemy + " dropped a potion.")
+            
+            time.sleep(1)
+            clear()
+
+
+
+
+
+
 
 
 
@@ -307,50 +392,81 @@ while run:
                     fight = True
                     battle()
 
-
-        draw()
-        print("LOCATION: " + biome [map[y][x]]["t"])
-        draw()
-        print("HP: " + str(HP) + "/" + str(HPMAX))
-        print("ATK: " + str(ATK))
-        print("POTIONS: " + str(pot))
-        print("ELIXIRS: " + str(elx))
-        print("GOLD: " + str(gold))
-        print("COORDS:", x, y)
-        draw()
-        print("0: SAVE AND QUIT")
-        if y > 0 and map[y-1][x] != "empty":
-                print("1 - NORTH")
-        if x < x_len and map[y][x+1] != "empty":
-            print("2 - EAST")
-        if y < y_len and map[y-1][x] != "empty":
-            print("3 - SOUTH")
-        if x > 0 and map[y][x-1] != "empty":
-            print("4 - WEST")
-        draw()
-
-        dest = input("> ")
-        if dest == "0":
-            play = False
-            menu = True
-            save()
-        elif dest == "1":
+        if play:
+            draw()
+            print("LOCATION: " + biome [map[y][x]]["t"])
+            draw()
+            print("HP: " + str(HP) + "/" + str(HPMAX))
+            print("ATK: " + str(ATK))
+            print("POTIONS: " + str(pot))
+            print("ELIXIRS: " + str(elx))
+            print("GOLD: " + str(gold))
+            print("COORDS:", x, y)
+            draw()
+            print("0: SAVE AND QUIT")
             if y > 0 and map[y-1][x] != "empty":
-                y -= 1
-                standing = False
-
-        elif dest == "2":
+                    print("1 - NORTH")
             if x < x_len and map[y][x+1] != "empty":
-                x += 1
-                standing = False
-
-        elif dest == "3":
-            if y < y_len and map[y+1][x] != "empty":
-                y += 1
-                standing = False
-
-        elif dest == "4":
+                print("2 - EAST")
+            if y < y_len and map[y-1][x] != "empty":
+                print("3 - SOUTH")
             if x > 0 and map[y][x-1] != "empty":
-                x -= 1
-                standing = False
+                print("4 - WEST")
+            if pot > 0:
+                print("5 - USE POTION")
+            if elx > 0:
+                print("6 - USE ELIXIR")
+            if map[y][x] == "shop" or map[y][x] == "mayor" or map[y][x] == "cave":
+                print("? - ENTER")
+            draw()
+
+            dest = input("> ")
+            if dest == "0":
+                play = False
+                menu = True
+                save()
+            elif dest == "1":
+                if y > 0 and map[y-1][x] != "empty":
+                    y -= 1
+                    standing = False
+
+            elif dest == "2":
+                if x < x_len and map[y][x+1] != "empty":
+                    x += 1
+                    standing = False
+
+            elif dest == "3":
+                if y < y_len and map[y+1][x] != "empty":
+                    y += 1
+                    standing = False
+
+            elif dest == "4":
+                if x > 0 and map[y][x-1] != "empty":
+                    x -= 1
+                    standing = False
+            
+            elif dest == "5":
+                if pot > 0:
+                    pot -= 1
+                    heal(30)
+                    hp -= atk
+                    print(enemy + " dealt " + str(atk) + " damage to " + name)
+                else:
+                    print("You have no potions left.")
+                input("> ")
+                standing = True
+            
+            elif dest == "6":
+                if elx > 0:
+                    elx -= 1
+                    heal(50)
+                    hp -= atk
+                    print(enemy + " dealt " + str(atk) + " damage to " + name)
+                else:
+                    print("You have no elixirs left.")
+                input("> ")
+                standing = True
+            else:
+                standing = True
+            
             
